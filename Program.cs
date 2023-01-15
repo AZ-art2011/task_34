9a07f49d3efab6b18ef97f76b449e2383101a2c3
Alexander <mypost@example.com>
2023-01-15 16:25:31 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// task 34
+
+Console.Clear();
+int val = 0;
+int[] RandArray = new int[10];
+
+for (int i=0; i<RandArray.Length; i++)
+{
+    RandArray[i] = new Random().Next(100, 1000);
+    if (RandArray[i]%2 == 0) val++;
+}
+
+Console.Write($" [ {String.Join(", ", RandArray)}]");
+Console.Write($" -> {val} четных чисел");
